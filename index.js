6ac53435b4e5de34a929e97130de486b3aa0595b
kavy pathak <pathakkavya022@gmail.com>
2026-01-18 01:09:20 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("harry")
+let a="harry";
+console.log(a[0])
+console.log(a[3])//print the characters by using these 
+console.log(a.length)//tells the length of string
+let  w="aditi"
+let r="kavya"
+console.log("her name is "+ w +" his freind is" + r)//trmplate letrals
+let b="kite"
+console.log(b.toLocaleLowerCase())
+console.log(b.toUpperCase())
+console.log(b.length)
+console.log(b.slice(1,2))
+console.log(b.replace("kite","aditi"))
+console.log(b.concat(a,"harry"))//combines as a is harry given and also combines new member 
+console.log(b)//cant change string once you make string and as soon as string allocates in memory but can make new string instead
+console.log(b.endsWith("e"))
+console.log(b.startsWith("k"))
+ 
